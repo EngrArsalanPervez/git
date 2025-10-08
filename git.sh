5bd81c37fe38e90815f3b23dc120cace9f0c4887
Arsalan Pervez <usman.pervez10@gmail.com>
2025-10-08 19:20:30 +0500

    Update git.sh

--- git.sh
+++ git.sh
@@ -99,6 +99,7 @@ git lfs ls-files
 echo 1024 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
 rm -rf build/
 sudo ./container/install_pace2.sh dev
+sudo export LD_LIBRARY_PATH=/usr/local/lib/pace2/lib:/usr/local/lib:$LD_LIBRARY_PATH
 meson setup build --wipe -Denable_dpi_qosmos=true -Denable_dpi_pace2=false      # Qosmos
 meson setup build --wipe -Denable_dpi_qosmos=false -Denable_dpi_pace2=true      # PACE2
 ninja -C build
