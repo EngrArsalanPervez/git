cf571b3c07bdf6a9820a537c2cd8a352bb733e1e
Arsalan Pervez <usman.pervez10@gmail.com>
2026-01-07 13:14:06 +0500

    Update git.sh

--- git.sh
+++ git.sh
@@ -8,7 +8,14 @@ git config --global user.email "Usman.Pervez@31c.io"
 git config --global user.name "Usman Pervez"
 ssh -T git@git.corp.31c.io
 
+# Meld
 git config --global diff.tool meld
+
+# nvim -d
+git config --global diff.tool nvimdiff
+git config --global difftool.nvimdiff.cmd "nvim -d \$LOCAL \$REMOTE"
+git config --global difftool.prompt false
+
 
 # Status
 git status
